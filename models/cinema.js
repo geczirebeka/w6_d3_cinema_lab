66e9cfa3a814348d2a8dc28ec4c2dd19af500f55
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-22 13:37:21 +0100

    first test passed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,12 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.listOfTitles = function (films) {
+  const result = this.films.map((film) => {
+    return film.title;
+  })
+  return result;
+}
+
+
 module.exports = Cinema;
